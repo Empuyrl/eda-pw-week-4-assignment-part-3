65ef5e87a09ab0c275361e9d039f9ebc2d13c77b
User <user@Users-MacBook-Pro.local>
2023-04-14 14:38:52 -0500

    added a fullOf function and created arrays/changed wording to make console log work

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,34 +2,62 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 console.log(basket);
+console.log(maxItems);
 
-function addItem(item) {
-basket.push(item);
-return true
-}
-console.log(addItem);
+ function addItem(item) {
+    if (isFull() === false){
+    basket.push(item);
+    return true; }
+   else { 
+     return false;
+    }
+ } 
+ 
 
-function listItems(basket) {
-    for (i = 0; i < basket.length; i++) {
-        console.log(basket[i]);
+function listItems(array) {
+    for (i = 0; i < array.length; i++) {
+        console.log(array[i]);
     }
 }
-console.log(listItems);
-
-console.log(`Basket is ${basket}`);
+ 
+console.log(`Basket is`, basket);
 console.log('Adding noodles (expect true)', addItem('noddles'));
-console.log(`Basket is now ${basket}`);
+console.log('Basket is', basket);
 
-console.log(`Basket is ${basket}`);
+console.log('Basket is', basket);
 console.log('Adding shrimp (expect true)', addItem('shrimp'));
-console.log(`Basket is now ${basket}`);
+console.log('Basket is', basket);
 
-console.log(`Basket is ${basket}`);
-console.log('Adding alfredo sauce (expect true)', addItem('alfredo sauce'));
-console.log(`Basket is now ${basket}`);
+console.log('Basket is', basket);
+console.log('Adding alfredo (expect true)', addItem('alfredo'));
+console.log('Basket is', basket);
+
+console.log('Basket is', basket);
+console.log('Adding spices (expect true)', addItem('spices'));
+console.log('Basket is', basket);
+
+console.log('Basket is', basket);
+console.log('Adding toast (expect true)', addItem('toast'));
+console.log('Basket is', basket);
+
+console.log('Basket is', basket);
+console.log('Adding peaches (expect false)', addItem('peaches'));
+console.log('Basket is', basket);
+
+function isFull() {
+    if(basket.length < maxItems) {
+        return false;
+    } else {
+        return true;
+    }
+}
+console.log(isFull());
 
 function empty() {
  basket = [];
 }
 console.log('The basket is now empty');
+
+
