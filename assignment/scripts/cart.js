4e02448a1adb5d7e62c02ddddc255083e70b7f2a
Jack Blonigen <jack.je.blonigen@gmail.com>
2023-04-14 11:22:00 -0500

    list and adding

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -9,3 +9,18 @@ basket.push(item);
 return true
 }
 console.log(addItem);
+
+function listItems(basket) {
+    for (i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+}
+console.log(listItems);
+
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+console.log(`Basket is ${basket}`);
+console.log('Adding caramel (expect true)', addItem('caramel'));
+console.log(`Basket is now ${basket}`);
