7fd81e6bc99a2b2d816f94f1d1a0394ddae49c3d
Jack Blonigen <jack.je.blonigen@gmail.com>
2023-04-14 08:55:13 -0500

    created a variable basket and a function for adding items to it

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,11 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+console.log(basket);
+
+function addItem(item) {
+basket.push(item);
+return true
+}
+console.log(addItem);
